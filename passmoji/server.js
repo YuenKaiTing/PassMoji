8f4f39b090dbfc34c861399d555bff645477ab47
Nik Harith <2673209b@student.gla.ac.uk>
2025-03-11 11:19:41 +0000

    added port env variable

--- passmoji/server.js
+++ passmoji/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const argon2 = require("argon2");
 const cors = require("cors");
 const path = require("path");
+require('dotenv').config();
 
 const app = express();
 
@@ -96,7 +97,7 @@ app.post("/api/login", async (req, res) => {
 });
 
 // Start server
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
